7413de5b1862ef57c7db90c50cb96f94863ff06b
sh7478 <sh7478@bs.amalnet.k12.il>
2026-01-03 22:24:49 +0200

    added javadoc to mainActivity.java

--- app/src/main/java/com/example/ex09121/MainActivity.java
+++ app/src/main/java/com/example/ex09121/MainActivity.java
@@ -20,6 +20,14 @@ import java.text.NumberFormat;
 public class MainActivity extends AppCompatActivity {
     TextView solution;
     EditText etNum1, etNum2;
+
+    /**
+     * Called when the activity is first created. This is where you should do all of your normal static set up: create views, bind data to lists, etc.
+     * <p>
+     *
+     * @param savedInstanceState If the activity is being re-initialized after previously being shut down then this Bundle contains the data it most recently supplied in onSaveInstanceState(Bundle). Note: Otherwise it is null.
+     * @return void
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,13 +37,31 @@ public class MainActivity extends AppCompatActivity {
         etNum1 = findViewById(R.id.etNum1);
         etNum2 = findViewById(R.id.etNum2);
     }
+    /**
+     * Called when the context menu for the view is being built.
+     * <p>
+     *
+     * @param menu The context menu that is being built
+     * @param v The view for which the context menu is being built
+     * @param menuInfo Extra information about the item for which the context menu should be shown. This information will vary depending on the class of v.
+     * @return void
+     */
+
     @Override
     public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
         menu.add("sum");
         menu.add("sub");
         menu.add("mul");
         menu.add("div");
-    }@Override
+    }
+    /**
+     * This hook is called whenever an item in a context menu is selected.
+     * <p>
+     *
+     * @param item The context menu item that was selected.
+     * @return boolean Returns true if the event was consumed, else false.
+     */
+    @Override
     public boolean onContextItemSelected(@NonNull MenuItem item) {
         String oper = item.getTitle().toString();
         if(tryParseDecimal(etNum1.getText().toString()) != null && tryParseDecimal(etNum2.getText().toString()) != null)
@@ -63,6 +89,13 @@ public class MainActivity extends AppCompatActivity {
         }
         return super.onContextItemSelected(item);
     }
+    /**
+     * Attempts to parse a string into a Double.
+     * <p>
+     *
+     * @param str The string to be parsed.
+     * @return Double Returns the parsed Double value if successful, or null if the string is not a valid number.
+     */
     public static Double tryParseDecimal(String str) {
         if (str == null || str.isEmpty()) {
             return null;
@@ -73,6 +106,13 @@ public class MainActivity extends AppCompatActivity {
         return null;
     }
 
+    /**
+     * Formats a double number into a specific string representation. It uses scientific notation for large numbers.
+     * <p>
+     *
+     * @param number The number to be formatted.
+     * @return String The formatted number as a string.
+     */
     public static String formatDecimal(double number) {
         DecimalFormat simpleFormat = new DecimalFormat("0.####");
         String formattedSimple = simpleFormat.format(number).replaceAll("[^0-9]", "");
@@ -87,6 +127,12 @@ public class MainActivity extends AppCompatActivity {
             return result.replace("E", "e");
         }
     }
+    /**
+     * Subtracts the second number from the first and displays the result in the solution TextView.
+     * <p>
+     *
+     * @return void
+     */
     private void subNums() {
         double num1 = Double.parseDouble(etNum1.getText().toString());
         double num2 = Double.parseDouble(etNum2.getText().toString());
@@ -94,12 +140,24 @@ public class MainActivity extends AppCompatActivity {
         solution.setText(formatDecimal(num1 - num2));
     }
 
+    /**
+     * Adds the two input numbers and displays the result in the solution TextView.
+     * <p>
+     *
+     * @return void
+     */
     private void sumNums() {
         double num1 = Double.parseDouble(etNum1.getText().toString());
         double num2 = Double.parseDouble(etNum2.getText().toString());
         solution.setText(formatDecimal(num1 + num2));
 
     }
+    /**
+     * Divides the first number by the second and displays the result. If the second number is zero, it displays an error message.
+     * <p>
+     *
+     * @return void
+     */
     private void divNums() {
         double num1 = Double.parseDouble(etNum1.getText().toString());
         double num2 = Double.parseDouble(etNum2.getText().toString());
@@ -112,6 +170,13 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+
+    /**
+     * Multiplies the two input numbers and displays the result in the solution TextView.
+     * <p>
+     *
+     * @return void
+     */
     private void mulNums() {
         double num1 = Double.parseDouble(etNum1.getText().toString());
         double num2 = Double.parseDouble(etNum2.getText().toString());
